1df49053901ee76fece022364c7a114e9edd3eff
lee <lee.ellenburg@nasa.gov>
2017-11-17 19:51:13 -0600

    update
    Added SAR but not used yet

--- KStest.py
+++ KStest.py
@@ -14,6 +14,12 @@ from scipy import stats
 import numpy
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+import statsmodels.tsa.stattools
+from scipy import signal
 #from PIL import ImageTk
 #import ee.mapclient
 
@@ -27,18 +33,20 @@ def addNDVI(image):
     #return image.addBands(ndvi)
     return ndvi.set("system:time_start",image.get("system:time_start"))
     
-def addNDVI2(image):
-    ndvi = image.normalizedDifference(['B5','B4']).rename(['nd'])
-    #ndvi = image.normalizedDifference(['nir', 'red'])
-    #return image.addBands(ndvi)
-    return ndvi.set("system:time_start",image.get("system:time_start"))
+def addEVI(image):
+   evi = image.expression(
+    '2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1))',
+    {'NIR': image.select(['nir']),
+    'RED': image.select(['red']),
+    'BLUE': image.select(['blue'])}).float().rename(['nd'])
+   return evi.set("system:time_start",image.get("system:time_start"))
     
 def lsCloudMask(img):
   blank = ee.Image(0)
   scored = ee.Algorithms.Landsat.simpleCloudScore(img)
   clouds = blank.where(scored.select(['cloud']).lte(cloudThresh),1)
   return img.updateMask(clouds).set("system:time_start",img.get("system:time_start"))
-''' 
+'''
 def viirsQuality(img):
     def getQABits(image, start, end, newName):
         # Compute the bits we need to extract.
@@ -53,7 +61,7 @@ def viirsQuality(img):
     qf = img.select('QF1')
     quality = getQABits(qf,0,3,'quality')
     return img.updateMask(quality.eq(3))
-'''  
+''' 
 # A helper to apply an expression and linearly rescale the output 
 def rescale(img, exp, thresholds):
   return img.expression(exp, {'img': img})\
@@ -88,10 +96,9 @@ def s2CloudMask(img):
   
   
 def mergeOptical(studyArea,t1,t2):
-    #viirsrename = viirs.filterBounds(studyArea)\
-                    #.filterDate(t1,t2)\
-                    #.select(['M5','M5','M5','M7','M5','M5'],['blue','green','red','nir','swir1','swir2'])
-
+    viirsrename = viirs.filterBounds(studyArea)\
+                    .filterDate(t1,t2)\
+                    .select(['M5','M5','M5','M7','M5','M5'],['blue','green','red','nir','swir1','swir2'])
                     #.map(viirsQuality)
            
     le7rename = le7.filterBounds(studyArea)\
@@ -114,8 +121,10 @@ def mergeOptical(studyArea,t1,t2):
                             ['blue','green','red','nir','swir1','swir2'])\
                     .map(bandPassAdjustment)
                     
-    out = ee.ImageCollection(le7rename.merge(lc8rename).merge(st2rename))#.merge(viirsrename))
-    
+    out = ee.ImageCollection(le7rename.merge(lc8rename))#.merge(st2rename))#.merge(viirsrename))
+    out = ee.ImageCollection(st2rename)#.merge(lc8rename))
+    out = ee.ImageCollection(viirsrename)
+    out = ee.ImageCollection(le7rename.merge(lc8rename).merge(st2rename))            
     return out
 
 
@@ -217,8 +226,167 @@ def simpleTDOM2(collection,zScoreThresh,shadowSumThresh,dilatePixels):
     
     return collection
 
+# add VV/VH channel to the SAR image
+def MWVImap(img):
+  vv = img.select('sum');
+  #var vvvh = vv.rename(['vegindex']);
+  vvvh = vv.multiply(0.0679).add(1.1221).rename(['nd']);
+  return img.addBands(vvvh);
+
+def toNatural(img):
+  return ee.Image(10.0).pow(img.select(0).divide(10.0))
+
+def toDB(img):
+  return ee.Image(img).log10().multiply(10.0)
     
-iniDate = '2010-01-01'
+def despeckle(img):
+  t = ee.Date(img.get('system:time_start'))
+  angles = img.select('angle')
+  # The RL speckle filter
+  img = toNatural(img)
+  # img must be in natural units, i.e. not in dB!
+  # Set up 3x3 kernels 
+  weights3 = ee.List.repeat(ee.List.repeat(1,3),3)
+  kernel3 = ee.Kernel.fixed(3,3, weights3, 1, 1, False)
+
+  mean3 = img.reduceNeighborhood(ee.Reducer.mean(), kernel3)
+  variance3 = img.reduceNeighborhood(ee.Reducer.variance(), kernel3)
+
+  # Use a sample of the 3x3 windows inside a 7x7 windows to determine gradients and directions
+  sample_weights = ee.List([[0,0,0,0,0,0,0], [0,1,0,1,0,1,0],[0,0,0,0,0,0,0],\
+                            [0,1,0,1,0,1,0], [0,0,0,0,0,0,0], [0,1,0,1,0,1,0],[0,0,0,0,0,0,0]])
+
+  sample_kernel = ee.Kernel.fixed(7,7, sample_weights, 3,3, False)
+
+  # Calculate mean and variance for the sampled windows and store as 9 bands
+  sample_mean = mean3.neighborhoodToBands(sample_kernel) 
+  sample_var = variance3.neighborhoodToBands(sample_kernel)
+
+  # Determine the 4 gradients for the sampled windows
+  gradients = sample_mean.select(1).subtract(sample_mean.select(7)).abs()
+  gradients = gradients.addBands(sample_mean.select(6).subtract(sample_mean.select(2)).abs())
+  gradients = gradients.addBands(sample_mean.select(3).subtract(sample_mean.select(5)).abs())
+  gradients = gradients.addBands(sample_mean.select(0).subtract(sample_mean.select(8)).abs())
+
+  # And find the maximum gradient amongst gradient bands
+  max_gradient = gradients.reduce(ee.Reducer.max())
+
+  # Create a mask for band pixels that are the maximum gradient
+  gradmask = gradients.eq(max_gradient)
+
+  # duplicate gradmask bands: each gradient represents 2 directions
+  gradmask = gradmask.addBands(gradmask)
+
+  # Determine the 8 directions
+  directions = sample_mean.select(1).subtract(sample_mean.select(4)).gt(sample_mean.select(4).\
+                                  subtract(sample_mean.select(7))).multiply(1)
+  
+  directions = directions.addBands(sample_mean.select(6).subtract(sample_mean.select(4)).\
+                                   gt(sample_mean.select(4).subtract(sample_mean.select(2))).multiply(2))
+  
+  directions = directions.addBands(sample_mean.select(3).subtract(sample_mean.select(4)).\
+                                   gt(sample_mean.select(4).subtract(sample_mean.select(5))).multiply(3))
+  
+  directions = directions.addBands(sample_mean.select(0).subtract(sample_mean.select(4)).\
+                                   gt(sample_mean.select(4).subtract(sample_mean.select(8))).multiply(4))
+  # The next 4 are the not() of the previous 4
+  directions = directions.addBands(directions.select(0).Not().multiply(5))
+  directions = directions.addBands(directions.select(1).Not().multiply(6))
+  directions = directions.addBands(directions.select(2).Not().multiply(7))
+  directions = directions.addBands(directions.select(3).Not().multiply(8))
+
+  # Mask all values that are not 1-8
+  directions = directions.updateMask(gradmask)
+
+  # "collapse" the stack into a singe band image (due to masking, each pixel has just one value (1-8) in it's directional band, and is otherwise masked)
+  directions = directions.reduce(ee.Reducer.sum())  
+
+  sample_stats = sample_var.divide(sample_mean.multiply(sample_mean))
+
+  # Calculate localNoiseVariance
+  sigmaV = sample_stats.toArray().arraySort().arraySlice(0,0,5).arrayReduce(ee.Reducer.mean(), [0])
+
+  # Set up the 7*7 kernels for directional statistics
+  rect_weights = ee.List.repeat(ee.List.repeat(0,7),3).cat(ee.List.repeat(ee.List.repeat(1,7),4))
+
+  diag_weights = ee.List([[1,0,0,0,0,0,0], [1,1,0,0,0,0,0], [1,1,1,0,0,0,0],\
+                          [1,1,1,1,0,0,0], [1,1,1,1,1,0,0], [1,1,1,1,1,1,0], [1,1,1,1,1,1,1]]);
+
+  rect_kernel = ee.Kernel.fixed(7,7, rect_weights, 3, 3, False)
+  diag_kernel = ee.Kernel.fixed(7,7, diag_weights, 3, 3, False)
+
+  # Create stacks for mean and variance using the original kernels. Mask with relevant direction.
+  dir_mean = img.reduceNeighborhood(ee.Reducer.mean(), rect_kernel).updateMask(directions.eq(1))
+  dir_var = img.reduceNeighborhood(ee.Reducer.variance(), rect_kernel).updateMask(directions.eq(1))
+
+  dir_mean = dir_mean.addBands(img.reduceNeighborhood(ee.Reducer.mean(), diag_kernel).updateMask(directions.eq(2)))
+  dir_var = dir_var.addBands(img.reduceNeighborhood(ee.Reducer.variance(), diag_kernel).updateMask(directions.eq(2)))
+
+  # and add the bands for rotated kernels
+  i = 1
+  while i < 4:
+    dir_mean = dir_mean.addBands(img.reduceNeighborhood(ee.Reducer.mean(), rect_kernel.rotate(i)).updateMask(directions.eq(2*i+1)))
+    dir_var = dir_var.addBands(img.reduceNeighborhood(ee.Reducer.variance(), rect_kernel.rotate(i)).updateMask(directions.eq(2*i+1)))
+    dir_mean = dir_mean.addBands(img.reduceNeighborhood(ee.Reducer.mean(), diag_kernel.rotate(i)).updateMask(directions.eq(2*i+2)))
+    dir_var = dir_var.addBands(img.reduceNeighborhood(ee.Reducer.variance(), diag_kernel.rotate(i)).updateMask(directions.eq(2*i+2)))
+    i+=1
+  # "collapse" the stack into a single band image (due to masking, each pixel has just one value in it's directional band, and is otherwise masked)
+  dir_mean = dir_mean.reduce(ee.Reducer.sum())
+  dir_var = dir_var.reduce(ee.Reducer.sum())
+
+  # A finally generate the filtered value
+  varX = dir_var.subtract(dir_mean.multiply(dir_mean).multiply(sigmaV)).divide(sigmaV.add(1.0))
+
+  b = varX.divide(dir_var)
+  
+  # Get a multi-band image bands.
+  result = dir_mean.add(b.multiply(img.subtract(dir_mean))).arrayProject([0])\
+    .arrayFlatten([['sum']])\
+    .float()
+
+
+  return toDB(result).addBands(angles).set('system:time_start',t.millis())
+'''
+# Funtion to normalize backscatter values to a nominal angle
+# using methods from Nguyen et al., 2015 doi:10.3390/rs71215808
+def normalizeBackscatter(img):
+  samp = img.sample({
+    'region': img.geometry(),
+    'scale': 30,
+    'numPixels': 250
+  });
+  angles = ee.Array(samp.aggregate_array(['angle']));
+  dB = ee.Array(samp.aggregate_array(sarBands[0]));
+
+  coefs = fitOLS(angles,dB);
+  print(coefs);
+
+  norm = ee.Image(30);
+  angleRatio = img.select(['angle']).subtract(norm).multiply(coefs.get([0]));
+  normBackscatter = img.select(['VV']).subtract(angleRatio);
+  return img.select(['angle']).addBands(normBackscatter);
+'''
+
+ 
+def normalizeBackscatter(img):
+  norm = ee.Image(30)
+  angleRatio = norm.divide(img.select(['angle']))
+  normBackscatter = img.select(['sum']).divide(angleRatio)
+
+  return img.select(['angle']).addBands(normBackscatter)
+  
+def microwv(geo,start,end, pol):
+    sar = s1.filterBounds(geo).filterDate(start,end).select([pol,'angle'])\
+                .filterMetadata('orbitProperties_pass','equals','ASCENDING').select([pol,'angle'])\
+                .map(despeckle).map(normalizeBackscatter).map(MWVImap)
+
+#              .filterMetadata('transmitterReceiverPolarisation','equals',['VV','VH'])\
+    #sar = sar.cast({'nd':'float', 'angle':'float', 'sum':'float'}, ['nd', 'angle', 'sum'])
+    return sar
+
+##############################################################################
+    
+iniDate = '2015-01-01'
 endDate = '2016-12-31'
 
 metadataCloudCoverMax = 75
@@ -228,9 +396,14 @@ cloudHeights = ee.List.sequence(200,5000,500)
 zScoreThresh = -0.75
 shadowSumThresh = 0.35
 windowSize = 15
-                    
-geo1 = ee.Geometry.Point([-93.61124038696289,42.162003755958466])
-geo2 = ee.Geometry.Point([-93.49742889404297,42.145078043817534])
+
+geoms = pd.read_csv('geo.csv')
+cols = geoms.columns[3:]
+col = 1
+
+                 
+#geo1 = ee.Geometry.Point([-93.61124038696289,42.162003755958466])
+#geo2 = ee.Geometry.Point([-93.49742889404297,42.145078043817534])
 
 viirs = ee.ImageCollection('NOAA/VIIRS/VNP09GA/001')
 lc8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_RT_TOA')
@@ -238,28 +411,21 @@ le7 = ee.ImageCollection('LANDSAT/LE07/C01/T1_RT_TOA')
 s2 = ee.ImageCollection('COPERNICUS/S2')
 s1 = ee.ImageCollection('COPERNICUS/S1_GRD')
 
-optical = mergeOptical(geo1, iniDate, endDate)
+#optical = mergeOptical(geo1, iniDate, endDate)
 #mwvVi = microwv(geo,iniDate, endDate).select(sarBand)
 
-'''
-def microwv(geo,start,end):
-    sar = s1.filterBounds(geo).filterDate(start,end)\
-              .filterMetadata('transmitterReceiverPolarisation','equals',['VV','VH'])\
-              .filterMetadata('orbitProperties_pass','equals','DESCENDING').select(['VV','angle'])\
-              .map(despeckle).map(normalizeBackscatter).map(calcVVVHBand)
-    return sar
-'''
-
 #sys.exit()
-optical = simpleTDOM2(optical,zScoreThresh,shadowSumThresh,dilatePixels);
+#optical = simpleTDOM2(optical,zScoreThresh,shadowSumThresh,dilatePixels)
+
+#vi = optical.map(addNDVI).select('nd')
+#mwvVi = microwv.select('vegindex')
+
+#imgSeries = ee.ImageCollection(vi.merge(mwvVi))
 #sys.exit(1)
 
-    
-raw = lc8.filterDate('2015-01-01', '2015-10-30')#.filterBounds(geometry)
-
-vi = optical.map(addNDVI).select('nd')
-vi2 = raw.map(addNDVI2).select('nd')
-
+  
+#raw = lc8.filterDate('2015-01-01', '2015-10-30')#.filterBounds(geometry)
+#vi2 = raw.map(addNDVI2).select('nd')
 
 
 nd = pd.DataFrame()
@@ -268,55 +434,127 @@ nd = pd.DataFrame()
 
 
 i = 0
-for geo in [geo1, geo2]:
+for row in geoms.loc[geoms[cols[col]] == 1][['Lat','Lon']].iterrows():
+#for geo in [geo1, geo2]:
+    
+    geo = ee.Geometry.Point([row[1]['Lon'],row[1]['Lat']])
+    
+    optical = mergeOptical(geo, iniDate, endDate)
+    optical = simpleTDOM2(optical,zScoreThresh,shadowSumThresh,dilatePixels)
+    optVi = optical.map(addNDVI).select('nd')
+    
+    mwvVi = microwv(geo,iniDate,endDate,'VH').select('nd')
+    
+    vi = ee.ImageCollection(optVi.merge(mwvVi))
+    
+    #sys.exit(0)
+    
     
     #optical = mergeOptical(geo, iniDate, endDate)
     
     #vi = optical.map(addNDVI).select('nd')
     # region values as generated by getRegion
-    region = vi.getRegion(geo, 30).getInfo()
+    regionO = optVi.getRegion(geo, 30).getInfo()
+    regionM = mwvVi.getRegion(geo, 30).getInfo()
+    #region = vi.getRegion(geo, 30).getInfo()
+    
+    #sys.exit()
+    
     
     # stuff the values in a dataframe for convenience      
-    df = pd.DataFrame.from_records(region[1:len(region)])
+    df1 = pd.DataFrame.from_records(regionO[1:len(regionO)])
     # use the first list item as column names
-    df.columns = region[0]
+    df1.columns = regionO[0]
+
+    df2 = pd.DataFrame.from_records(regionM[1:len(regionM)])
+    # use the first list item as column names
+    df2.columns = regionM[0]
+    #sys.exit()
     
     # divide the time field by 1000 as in milliseconds
     # while datetime takes seconds to convert unix time
     # to dates
-    df.time = df.time / 1000
-    df['time'] = pd.to_datetime(df['time'], unit = 's')
-    df.rename(columns = {'time': 'date'}, inplace = True)
-    df = df.sort_values(by = 'date')
+    df1.time = df1.time / 1000
+    df1['time'] = pd.to_datetime(df1['time'], unit = 's')
+    df1.rename(columns = {'time': 'date'}, inplace = True)
+    df1 = df1.sort_values(by = 'date')
     #sys.exit()
-    df.index = df['date']
+    del df1['id']
+    df1 = df1.groupby(df1['date']).mean()
+    #df1.index = df1['date']
+    #df1 = df1.drop_duplicates(keep='last')
+    #df1 = df1.groupby(pd.TimeGrouper(freq='d')).mean()
+    
+    df2.time = df2.time / 1000
+    df2['time'] = pd.to_datetime(df2['time'], unit = 's')
+    df2.rename(columns = {'time': 'date'}, inplace = True)
+    df2 = df2.sort_values(by = 'date')
+    del df2['id']
+    df2 = df2.groupby(df2['date']).mean()
+    #df2.index = df2['date']
+    #df2 = df2.drop_duplicates(keep='last')
+    #df2 = df2.groupby(pd.TimeGrouper(freq='d')).mean()
+        
+    ## Regress backscatter to evi   
+    a = pd.DataFrame(signal.detrend(df1['nd'].dropna()), index = df1['nd'].dropna().index)
+    c = pd.DataFrame(signal.detrend(df2['nd'].dropna()), index = df2['nd'].dropna().index)
+    
+    mo = pd.concat([a.iloc[:,0],c.iloc[:,0]],1).groupby(pd.TimeGrouper(freq='M')).mean().dropna()
+    
+    #mo = pd.concat([df1['nd'],df2['nd']],1).groupby(pd.TimeGrouper(freq='M')).mean().dropna()
 
-    df['date_'+str(i)] = df['date']
-    del df['date']
+    #sys.exit()
+    m,b,r,p,e = linregress(mo.iloc[:,1].values,mo.iloc[:,0].values)
+    
+    #newmo = 
+   
+    print r**2
+    
+    #df.iloc[:,1]*m+b
+    #sys.exit()
+    ser_no = df1['nd']
+    ser_no = pd.DataFrame(signal.detrend(df1['nd'].dropna()), index = df1['nd'].dropna().index)
+    ser_no.columns = ['nd']
+
+    ser = pd.concat([df1['nd'],df2['nd']*m+b])
+    
+    df = pd.DataFrame(ser_no)
+    
+    #df['date_'+str(i)] = df['date']
+    #del df['date']
     df['nd_'+str(i)] = df['nd']
     del df['nd']
     #sys.exit()  
-    df['interp_'+str(i)] = df['nd_'+str(i)].interpolate('nearest')
-    df['smooth_'+str(i)] = smooth(df['interp_'+str(i)], window_len=6)[2:-3]
     
     if i == 0:
         nd = df
     else:
         nd = pd.concat([nd,df],axis=1)
-       
-    i += 1
-
+    #sys.exit()
     
+    i += 1
+plist = []
+for j in np.arange(i):  
+    nd['interp_'+str(j)] = nd['nd_'+str(j)].interpolate('nearest')
+for j in np.arange(i):
+    nd['smooth_'+str(j)] = smooth(nd['interp_'+str(j)], window_len=30)[15:-14]
+       
+    plist.append('smooth_'+str(j))
+        
+nd.iloc[:,12:].plot()
+
+       
+       
 #nd[['nd_0','nd_1']].plot(linestyle = '-')
 
 #statsmodels.tsa.stattools.grangercausalitytests(nd[['nd_0','nd_1']], 5, addconst=True, verbose=True)
 
 #filled in nans 
 # = nd.fillna(method='ffill')
-
-
-
-
+'''
+nd['smooth_0'].plot(marker = '.')
+nd['smooth_1'].plot(marker = '.')
+'''
 '''
 
 var optical = mergeOptical(geo, iniDate, endDate);
@@ -334,3 +572,19 @@ Map.addLayer(filtered.median(), rgb_vis, 'RGB (median)');
 Map.addLayer(greenest, rgb_vis, 'RGB (greenest pixel)');
 print(Chart.image.series(with_ndvi.select('nd'), roi));
 '''
+
+'''
+    b = []
+    i = 0
+    while i < len(regionM[1:]):
+        a = regionM[i+1][3]
+        j = 0
+        while j < len(regionO[1:]):
+            if float(regionM[i+1][3]) > regionO[j+1][3]-864000000.0 and a < regionO[j+1][3]+864000000.0:
+                print regionO[j+1][3]
+                b.append([regionM[i+1][4],regionO[j+1][4]])
+                #print a,regionO[j+1][3]
+                #sys.exit()
+            j+=1   
+        i+=1
+    '''
